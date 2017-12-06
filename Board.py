9f6292a5860eb9c59adfcf8937d3c6f2d6b4286c
Ryan Ly <ryanly.rl1@gmail.com>
2017-12-06 13:57:57 -0500

    Added checkmate checking

--- Board.py
+++ Board.py
@@ -4,12 +4,8 @@ from Piece import Piece
 
 class Board:
 
-    def __init__(self, window_size, display=True, board=None):
-        pygame.init()
+    def __init__(self, board=None):
         self.board = [[None for i in range(8)] for j in range(8)]
-        self.screen = pygame.display.set_mode(window_size)
-        self.square_size = (window_size[0] / 8, window_size[1] / 8)
-        pygame.display.update()
 
         if board:
             self.board = board
@@ -39,95 +35,7 @@ class Board:
             self.board[6][7] = Piece('knight', 'black_knight.png', 'black')
             self.board[7][7] = Piece('rook', 'black_rook.png', 'black')
 
-        if display:
-            self.draw_board()
-            pygame.display.update()
-            self.current_colour = 'black'
-            if display:
-                self.game_loop()
-
-    def draw_pieces(self):
-        for i in range(8):
-            for j in range(8):
-                if self.board[i][j] is not None:
-                    image = pygame.image.load(self.board[i][j].sprite)
-                    image = image.convert_alpha()
-                    self.screen.blit(image, (i*self.square_size[0], j*self.square_size[1]))
-
-    def draw_board(self):
-        for i in range(8):
-            if i % 2 == 0:
-                colour = (139,69,19)
-            else:
-                colour = (255,248,220)
-            for j in range(8):
-                if colour == (139,69,19):
-                    colour = (255,248,220)
-                else:
-                    colour = (139,69,19)
-                pygame.draw.rect(self.screen, colour, (j* self.square_size[0], i * self.square_size[1], self.square_size[0], self.square_size[1]))
-        self.draw_pieces()
-        pygame.display.update()
-
-    def game_loop(self):
-        currently_selected = None
-        currently_selected_moves = None
-        current_piece_pos = None
-        while True:
-            pygame.display.update()
-            event = pygame.event.poll()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print('click')
-                (pos_x, pos_y) = pygame.mouse.get_pos()
-                pos_x = int(pos_x // self.square_size[0])
-                pos_y = int(pos_y // self.square_size[1])
-                #print((pos_x, pos_y), currently_selected_moves)
-                if currently_selected is None:
-                    self.draw_board()
-
-                    if self.board[pos_x][pos_y] is not None and self.board[pos_x][pos_y].colour == self.current_colour:
-                        currently_selected = self.board[pos_x][pos_y]
-                        if currently_selected.type == 'king':
-                            currently_selected_moves = self.is_checkmate(self.current_colour, self.board)
-                        else:
-                            currently_selected_moves = self.board[pos_x][pos_y].get_moves(self.board, (pos_x, pos_y))
-                        current_piece_pos = (pos_x, pos_y)
-                        for (x,y) in currently_selected_moves:
-                            pygame.draw.rect(self.screen, (0,255,0), ((pos_x + x) * self.square_size[0], (pos_y + y) * self.square_size[1], self.square_size[0], self.square_size[1]))
-                            self.draw_pieces()
-                elif currently_selected is not None and currently_selected_moves is not None and current_piece_pos is not None:
-                    # move another piece
-                    if self.board[pos_x][pos_y] is not None and self.board[pos_x][pos_y].colour == self.current_colour:
-                        print('switching')
-                        currently_selected = self.board[pos_x][pos_y]
-                        currently_selected_moves = self.board[pos_x][pos_y].get_moves(self.board, (pos_x, pos_y))
-                        current_piece_pos = (pos_x, pos_y)
-                        self.draw_board()
-                        for (x, y) in currently_selected_moves:
-                            pygame.draw.rect(self.screen, (0, 255, 0), (
-                                (pos_x + x) * self.square_size[0], (pos_y + y) * self.square_size[1],
-                                self.square_size[0], self.square_size[1]))
-                            self.draw_pieces()
-                    for move in currently_selected_moves:
-                        print(current_piece_pos)
-                        if (current_piece_pos[0] + move[0], current_piece_pos[1] + move[1]) == (pos_x, pos_y):
-                            self.board[pos_x][pos_y] = currently_selected
-                            self.board[current_piece_pos[0]][current_piece_pos[1]] = None
-                            currently_selected.has_moved = True
-                            currently_selected = None
-                            currently_selected_moves = None
-                            self.draw_board()
-                            if self.current_colour == 'white':
-                                self.current_colour = 'black'
-                                print('Black turn')
-                            else:
-                                self.current_colour = 'white'
-                                print('White turn')
-
-                            if not self.is_checkmate(self.current_colour, self.board):
-                                print('checkmate')
-
-    def is_checkmate(self, colour, board):
+    def is_checkmate(self, colour, board, depth):
         opponent_possible_moves = []
         player_possible_moves = []
         king_pos = ()
@@ -149,18 +57,17 @@ class Board:
         king = board[king_pos[0]][king_pos[1]]
         if king_pos in opponent_possible_moves:
             print('check')
-            king_moves = []
             for move in king.get_moves(board, (king_pos[0], king_pos[1])):
                 if (king_pos[0] + move[0], king_pos[1] + move[1]) not in opponent_possible_moves:
-                    king_moves.append(move)
-            if not king_moves:
-                # test_board = Board((500,500), False, board)
-                # for move in player_possible_moves:
-                #     test_board.board[move[0]][move[1]] = None
-                #     if test_board.board[move[0]][move[1]] is not None and test_board.board[move[0]][move[1]].colour != colour:
-                #         if self.is_checkmate(colour, test_board):
-                #             print('can kill to escape checkmate')
-                print('checkmate')
-            return king_moves
+                    return False
+            if depth == 0:
+                for move in player_possible_moves:
+                    test_board = Board(self.board)
+                    test_board[move[0]][move[1]] = None
+                    if not self.is_checkmate(colour, test_board, 1):
+                        return False
+                    print('can kill to escape')
+            print('checkmate')
+            return True
         else:
-            return king.get_moves(board, (king_pos[0], king_pos[1]))
+            return True
--- ChessGame.py
+++ ChessGame.py
@@ -4,7 +4,99 @@ from Board import Board
 class ChessGame:
 
     def __init__(self, window_size):
-        self.board = Board(window_size)
+        pygame.init()
+        self.board = Board()
+        self.screen = pygame.display.set_mode(window_size)
+        self.square_size = (window_size[0] / 8, window_size[1] / 8)
+        pygame.display.update()
+        self.draw_board()
+        pygame.display.update()
+        self.current_colour = 'black'
+        self.game_loop()
+
+    def draw_pieces(self):
+        for i in range(8):
+            for j in range(8):
+                if self.board.board[i][j] is not None:
+                    image = pygame.image.load(self.board.board[i][j].sprite)
+                    image = image.convert_alpha()
+                    self.screen.blit(image, (i * self.square_size[0], j * self.square_size[1]))
+
+    def draw_board(self):
+        for i in range(8):
+            if i % 2 == 0:
+                colour = (139, 69, 19)
+            else:
+                colour = (255, 248, 220)
+            for j in range(8):
+                if colour == (139, 69, 19):
+                    colour = (255, 248, 220)
+                else:
+                    colour = (139, 69, 19)
+                pygame.draw.rect(self.screen, colour, (
+                j * self.square_size[0], i * self.square_size[1], self.square_size[0], self.square_size[1]))
+        self.draw_pieces()
+        pygame.display.update()
+
+    def game_loop(self):
+        currently_selected = None
+        currently_selected_moves = None
+        current_piece_pos = None
+        while True:
+            pygame.display.update()
+            event = pygame.event.poll()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print('click')
+                (pos_x, pos_y) = pygame.mouse.get_pos()
+                pos_x = int(pos_x // self.square_size[0])
+                pos_y = int(pos_y // self.square_size[1])
+                # print((pos_x, pos_y), currently_selected_moves)
+                if currently_selected is None:
+                    self.draw_board()
+
+                    if self.board.board[pos_x][pos_y] is not None and self.board.board[pos_x][pos_y].colour == self.current_colour:
+                        currently_selected = self.board.board[pos_x][pos_y]
+                        if currently_selected.type == 'king':
+                            currently_selected_moves = self.board.is_checkmate(self.current_colour, self.board.board)
+                        else:
+                            currently_selected_moves = self.board.board[pos_x][pos_y].get_moves(self.board.board, (pos_x, pos_y))
+                        current_piece_pos = (pos_x, pos_y)
+                        for (x, y) in currently_selected_moves:
+                            pygame.draw.rect(self.screen, (0, 255, 0), (
+                            (pos_x + x) * self.square_size[0], (pos_y + y) * self.square_size[1], self.square_size[0],
+                            self.square_size[1]))
+                            self.draw_pieces()
+                elif currently_selected is not None and currently_selected_moves is not None and current_piece_pos is not None:
+                    # move another piece
+                    if self.board.board[pos_x][pos_y] is not None and self.board.board[pos_x][pos_y].colour == self.current_colour:
+                        print('switching')
+                        currently_selected = self.board.board[pos_x][pos_y]
+                        currently_selected_moves = self.board.board[pos_x][pos_y].get_moves(self.board.board, (pos_x, pos_y))
+                        current_piece_pos = (pos_x, pos_y)
+                        self.draw_board()
+                        for (x, y) in currently_selected_moves:
+                            pygame.draw.rect(self.screen, (0, 255, 0), (
+                                (pos_x + x) * self.square_size[0], (pos_y + y) * self.square_size[1],
+                                self.square_size[0], self.square_size[1]))
+                            self.draw_pieces()
+                    for move in currently_selected_moves:
+                        print(current_piece_pos)
+                        if (current_piece_pos[0] + move[0], current_piece_pos[1] + move[1]) == (pos_x, pos_y):
+                            self.board.board[pos_x][pos_y] = currently_selected
+                            self.board.board[current_piece_pos[0]][current_piece_pos[1]] = None
+                            currently_selected.has_moved = True
+                            currently_selected = None
+                            currently_selected_moves = None
+                            self.draw_board()
+                            if self.current_colour == 'white':
+                                self.current_colour = 'black'
+                                print('Black turn')
+                            else:
+                                self.current_colour = 'white'
+                                print('White turn')
+
+                            if self.board.is_checkmate(self.current_colour, self.board.board, 0):
+                                print('checkmate')
 
     def start(self):
         pass
